720766475e9bda5a6d1c4c1c382a6b598e2532c6
Ioannis Gimiliaris <johngimis@gmail.com>
2022-07-05 10:53:15 +0300

    changed the top to include lat and lon on the screen

--- index.js
+++ index.js
@@ -4,32 +4,27 @@ import 'aframe';
 
 import { GoogleProjection } from 'jsfreemaplib';
 
-
-alert('jsrunning2221ab222111aaaaasssww2345678wdfuver111');
+//my form to check if the browser updates the version!
+alert('jsrunning2221ab222111aaaaasssww2345678wdfuver111ver1.2.21');
 
 
 
 AFRAME.registerComponent('peakf', {
 
     init: function () {
-
-
-
-        // Handle a GPS update ...
         window.addEventListener('gps-camera-update-position', async (e) => {
 
 
-
+            //welcome notice
 
             var text2 = document.createElement('div');
             text2.style.position = 'absolute';
-            //text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
-            text2.style.width = 100;
+            text2.style.width = 200;
             text2.style.height = 100;
-            text2.style.backgroundColor = "blue";
-            text2.innerHTML = "hi there!";
-            text2.style.top = 200 + 'px';
-            text2.style.left = 200 + 'px';
+            text2.style.backgroundColor = "white";
+            text2.innerHTML = "Welcome to the app. Feel free to click any of the points for a surprise!";
+            text2.style.top = 1 + 'px';
+            text2.style.left = 1 + 'px';
             document.body.appendChild(text2);
 
 
@@ -37,10 +32,26 @@ AFRAME.registerComponent('peakf', {
 
             const resp = await fetch(`https://hikar.org/webapp/map?bbox=${e.detail.position.longitude - 0.05},${e.detail.position.latitude - 0.05},${e.detail.position.longitude + 0.05},${e.detail.position.latitude + 0.05}&layers=poi&outProj=4326 `);
 
+
+            //coordinates box
+            var coordinates = document.createElement('div');
+            coordinates.style.position = 'absolute';
+
+            coordinates.style.width = 200;
+            coordinates.style.height = 100;
+            coordinates.style.backgroundColor = "white";
+            coordinates.innerHTML = `Your Current Latitude is: ${e.detail.position.latitude} and your current Longitude is: ${e.detail.position.longitude}`;
+            coordinates.style.top = 20 + 'px';
+            coordinates.style.left = 1 + 'px';
+            document.body.appendChild(coordinates);
+
             const points = await resp.json();
 
 
             points.features.forEach(point => {
+
+
+                // plane noticeboard ISSUE ISSUE NOT SHOWING UP IN THE END RESULT
 
                 const planeEntity = document.createElement('a-plane');
 
@@ -64,12 +75,12 @@ AFRAME.registerComponent('peakf', {
                 });
 
 
+
+                //SIMPLE TEXT
+
+
                 const textEntity = document.createElement('a-text');
 
-
-
-                // set the lat and lon of the entity by setting
-                // its gps-projected-entity place component
                 textEntity.setAttribute('gps-projected-entity-place', {
                     latitude: point.geometry.coordinates[1],
                     longitude: point.geometry.coordinates[0]
@@ -81,34 +92,17 @@ AFRAME.registerComponent('peakf', {
                     y: 1000,
                     z: 1000
                 });
-
-                // Make the text look-at the camera
                 textEntity.setAttribute('look-at', '[gps-projected-camera]');
-
-
-                // Horizontally centre-align the text so the centre of the
-                // text is placed at the parent entity's world position
                 textEntity.setAttribute('align', 'center');
                 textEntity.setAttribute('value', point.properties.name || 'No name');
 
-                // Add the text entity to the scene
+                // Add the  ENTITIES to the scene
 
 
                 this.el.sceneEl.appendChild(textEntity);
                 this.el.sceneEl.appendChild(planeEntity);
 
-
-
-
-
-
-
-
             });
-
-
-
-
 
         });
     }
